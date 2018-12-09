8e7edee71621cd85251d9f51eefbf9e6c4429b0f
hela <luah613@gmail.com>
2018-12-09 14:16:44 -0600

    landing page sumo abolengo

---
+++ themes/lifestone/footer-abolengo.php
@@ -0,0 +1,7 @@
+					</div>
+            <!--================= Start Footer ==================-->
+            
+        </div><!-- end of wrapper opened in header -->
+    <?php wp_footer(); ?>
+    </body>
+</html>
---
+++ themes/lifestone/page-sumo-abolengo.php
@@ -0,0 +1,209 @@
+<?php
+/*
+ * Template name: Sumo Abolengo
+*/
+get_header();
+?>
+
+<section id="sumo_abolengo">
+	<section class="section_one">
+		<div class="sumo_head">
+			<figure class="header_image">
+				<img src="<?php echo get_template_directory_uri().'/images/sabolengo/sumo.png' ?>" alt="Sumo Abolengo">
+			</figure>
+			<iframe width="640" height="360" src="https://www.youtube.com/embed/pRe06c6dEIU" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+		</div>
+		<nav class="sumo_navigation">
+			<ul>
+				<li>
+					<a href="#section_cuadrilatero">
+						<figure>
+							<img src="<?php echo get_template_directory_uri().'/images/sabolengo/nav_1.png'; ?>" alt="Cuadrilatero">
+						</figure>
+					</a>
+				</li>
+				<li>
+					<a href="#section_fina_mezcla">
+						<figure>
+							<img src="<?php echo get_template_directory_uri().'/images/sabolengo/nav_2.png'; ?>" alt="Fina Mezcla">
+						</figure>
+					</a>
+				</li>
+				<li>
+					<a href="#section_we_human">
+						<figure>
+							<img src="<?php echo get_template_directory_uri().'/images/sabolengo/nav_3.png'; ?>" alt="We Human">
+						</figure>
+					</a>
+				</li>
+				<li>
+					<a href="#section_hgrafica">
+						<figure>
+							<img src="<?php echo get_template_directory_uri().'/images/sabolengo/nav_4.png'; ?>" alt="Herencia Gráfica">
+						</figure>
+					</a>
+				</li>
+				<li>
+					<a href="#section_mp">
+						<figure>
+							<img src="<?php echo get_template_directory_uri().'/images/sabolengo/nav_5.png'; ?>" alt="M.P.">
+						</figure>
+					</a>
+				</li>
+				<li>
+					<a href="#section_tm_miedo">
+						<figure>
+							<img src="<?php echo get_template_directory_uri().'/images/sabolengo/nav_6.png'; ?>" alt="Todo Menos Miedo">
+						</figure>
+					</a>
+				</li>
+			</ul>
+		</nav>
+	</section>
+
+
+	<section id="section_cuadrilatero" class="section_figure">
+		<figure>
+			<img src="<?php echo get_template_directory_uri().'/images/sabolengo/cuadrilatero.png'; ?>" alt="Cuadrilatero">
+		</figure>
+		<div class="sumo_texto">
+			<p>La pura sustancia activa / </p>
+			<p>La mejor noche tiene todo menos miedo</p>
+			<p>Productora de eventos culturales y de entretenimiento para necesidades específicas</p>
+			<p>Conceptualizamos, desarrollamos y producimos eventos empresariales,<br>
+			académicos y personales con una sola premisa:</p>
+			<br>
+			<p>cada uno es especial porque es tuyo.</p>
+			<p>Cuéntanos qué quieres, qué necesitas y diseñamos tu celebración.</p>
+		</div>
+	</section>
+
+	<section id="section_fina_mezcla" class="section_figure">
+		<figure>
+			<img src="<?php echo get_template_directory_uri().'/images/sabolengo/fina_mezcla.png'; ?>" alt="Cuadrilatero">
+		</figure>
+		<div class="sumo_texto">
+			<p>
+				Producimos ideas sonoras en forma de podcast, que nacen de la<br> 
+				investigación académica o comercial; y todo material sonoro que<br>
+				necesites para :
+			</p>
+			<br>
+			<p>
+				ambientar eventos – congresos, simposios, encuentros -. También<br> 
+				conceptualizamos y realizamos spots y campañas de audio<br>
+				para radio e internet.
+			</p>
+			<br>
+			<p>
+				Sonorizamos trabajamos, diseñamos ideas.
+			</p>
+		</div>
+	</section>
+
+	<section id="section_we_human" class="section_figure">
+		<figure>
+			<img src="<?php echo get_template_directory_uri().'/images/sabolengo/we.png'; ?>" alt="Cuadrilatero">
+		</figure>
+		<div class="sumo_texto">
+			<p>
+				Diseños originales de impresos para diversas aplicaciones textiles.
+			</p>
+			<br>
+			<p>
+				Ropa de autores mexicanos en ediciones limitadas.
+			</p>
+			<br>
+			<p>
+				También realizamos trabajos sobre pedido y características especiales.
+			</p>
+		</div>
+	</section>
+
+	<section id="section_hgrafica" class="section_figure">
+		<figure>
+			<img src="<?php echo get_template_directory_uri().'/images/sabolengo/herencia_grafica.png'; ?>" alt="Cuadrilatero">
+		</figure>
+		<div class="sumo_texto">
+			<p>
+				Taller de artes gráficas e impresiones multiformato.
+			</p>
+			<br>
+			<p>
+				Desde libros, etiquetas, presentaciones, textiles:
+			</p>
+			<br>
+			<p>
+				todo aquello que va impreso lleva nuestra huella.
+			</p>
+		</div>
+	</section>
+
+	<section id="section_mp" class="section_figure">
+		<figure>
+			<img src="<?php echo get_template_directory_uri().'/images/sabolengo/mp.png'; ?>" alt="Cuadrilatero">
+		</figure>
+		<div class="sumo_texto">
+			<p>
+				Servicios editoriales multipropósito.
+			</p>
+			<p>
+				¿Tienes una idea, tienes una entrega, tienes un texto?
+			</p>
+			<br>
+			<p>
+				Lo corregimos, lo editamos y damos formato editorial<br>
+				(sí el diseño va incluido)
+			</p>
+			<br>
+			<p>
+				Realizamos manuales de venta, estilo y marca;<br>
+				catálogos (impresos y web) y libros personales y de ocasión.
+			</p>
+			<br>
+			<p>
+				También: anuarios, boletines y revistas.
+			</p>
+			<br>
+			<p>
+				Contamos con un consejo editorial y de arte para asesorías y consultorías.
+			</p>
+		</div>
+	</section>
+
+	<section id="section_tm_miedo" class="section_figure">
+		<figure>
+			<img src="<?php echo get_template_directory_uri().'/images/sabolengo/tt.png'; ?>" alt="Cuadrilatero">
+		</figure>
+		<div class="sumo_texto">
+			<p>
+				No es una esquina es un vértice.
+			</p>
+			<br>
+			<p>	
+				Agencia y productora audiovisual que desarrolla contenidos<br> 
+				creativos a partir de la optimización de recursos para potenciar <br>
+				resultados.
+			</p>
+			<br>
+			<p>
+				Concepción, desarrollo y producción de estrategias comunicativas.
+			</p>
+		</div>
+	</section>
+
+	<section class="section_sumo_footer">
+		<figure>
+			<img src="<?php echo get_template_directory_uri().'/images/sabolengo/sa2.png'; ?>">
+			<figcaption>
+				<div class="sumo_texto">
+					<p>
+						<a href="mailto:contacto@sumoabolengo.com">CONTACTO@SUMOABOLENGO.COM</a>
+					</p>
+				</div>
+			</figcaption>
+		</figure>
+	</section>
+
+</section>
+<?php get_footer(); ?>
